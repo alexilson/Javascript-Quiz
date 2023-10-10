19ab2ae04053f7dd6e8997065e84483a02299dee
Alex <alex.ilson@outlook.com>
2023-10-09 21:53:53 -0700

    added questions object, answer set, change to button to say stop game after game begins

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,28 +1,77 @@
 const startBtn = document.querySelector("#start_button");
 const countEl = document.querySelector("#count");
+const questionTitle = document.querySelector("#question_title");
+// const questionText = document.querySelector("#question_text");
+// const questionForm = document.querySelector("#question_form");
 let gameState = false;
 
 
+// Question and answer text provided by Xpert Learning Assitant
 const questionSet = {
     questions: {
-        
-    }
+            1: {
+                title: "Question 1",
+                text:  "What is the correct way to declare a variable in JavaScript?",
+                a: "a) let myVar = 10;",
+                b: "b) var myVar = 10;",
+                c: "c) const myVar = 10;",
+                d: "d) JavaScript doesn't believe in variables, it prefers magic spells."
+            },
+
+            2: {
+                title: "Question 2:",
+                text:  "Which of the following is NOT a JavaScript data type?",
+                a: "a) String",
+                b: "b) Number",
+                c: "c) Boolean",
+                d: "d) Pizza"
+            },
+
+            3: {
+                title: "Question 3:",
+                text:  "What does the "===" operator in JavaScript do?",
+                a: "a) Compares two values for equality, without type conversion",
+                b: "b) Assigns a value to a variable",
+                c: "c) Performs a mathematical operation",
+                d: "d) Makes JavaScript sing a happy song"
+            },
+
+            4: {
+                title: "Question 4:",
+                text:  "What does the \"DOM\" stand for in JavaScript?",
+                a: "a) Document Object Model",
+                b: "b) Dancing Octopus Mascot",
+                c: "c) Donuts Over Muffins",
+                d: "d) Definitely Outrageous Monkeys"
+            },
+
+            5: {                
+                title: "Question 5:",
+                text:  "What is the purpose of the \"forEach\" method in JavaScript?",
+                a: "a) To loop through an array and execute a function on each element",
+                b: "b) To summon a friendly dragon",
+                c: "c) To calculate the square root of a number",
+                d: "d) To make your computer emit a delicious smell"
+            }
+},
+answerSet: 
+    ["a", "d", "a", "a", "a"]
 }
 
 function displayQuestion() {
-    
+//     for (let i = 1; i = Object.keys(questionSet.questions).length; ++i) {
+//     questionTitle.textContent = "Hello"; //questionSet.questions.i.title.value;
+// }
 }
 
 
 function displayCount(current_count) {
-    // console.log(current_count);
-    countEl.innerHTML = "Time: " + current_count + " seconds"; 
+    countEl.innerHTML = current_count; 
 }
 
 
 function startTimer() {
-    gameState = true;
-    let count = 0;
+    let count = 1;
     intervalId = setInterval(
         function() {
             displayCount(count);
@@ -34,7 +83,7 @@ function startTimer() {
 
 function stopTimer() {
     gameState = false;
-    clearInterval(intervalId); // got this from Xpert AI
+    clearInterval(intervalId); 
     let count = 0;
     displayCount(count);
 }
@@ -42,7 +91,9 @@ function stopTimer() {
 
 startBtn.addEventListener('click', function() {
     if (gameState == false) {
-        startTimer();
+        gameState = true;
+        startTimer(); // starts the timer
+        startBtn.innerHTML = "Stop Game"; // change button to say Stop Game
         displayQuestion();
     }
     else if (gameState == true) {
@@ -50,3 +101,5 @@ startBtn.addEventListener('click', function() {
     }
 })
 
+// I could move all this to a function called startGame where it changes the gamestate, starts the timer, and
+// changes the button text to read Stop Timer.
